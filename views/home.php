6de85d2f47e848675fe244015d485b188c845f0f
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-10-05 13:35:11 -0500

    creando la vista del login

--- views/home.php
+++ views/home.php
@@ -1,1 +1,4 @@
-home
+<div>
+    <h1 class="title is-fluid">Home</h1>
+    <h2 class="subtitle">Bienvenido Usuario</h2>
+</div>
--- views/login.php
+++ views/login.php
@@ -1,1 +1,19 @@
-login
+<div class="container">
+    <form class="box">
+        <h3 class="title is-2 is-spaced has-text-centered">Sistema de Inventario</h3>
+        <div class="field">
+            <label class="label subtitle is-5">Usuario</label>
+            <div class="control">
+                <input class="input" type="email" placeholder="e.g. alex@example.com"/>
+            </div>
+        </div>
+
+        <div class="field">
+            <label class="label subtitle is-5">Password</label>
+            <div class="control">
+                <input class="input" type="password" placeholder="********"/>
+            </div>
+        </div>
+        <button class="button is-primary">Ingresar al Sistema</button>
+    </form>
+</div>
