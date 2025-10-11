2c8945314a65ce64ed55efff4fa47f16d0f398c9
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-10-11 09:43:29 -0500

    FUNCION PHP para EVITAR INYECCION SQL

--- php/main.php
+++ php/main.php
@@ -21,9 +21,45 @@ function verificar_datos($filtro, $cadena)
         return true;
     }
 }
+
 /*
 $nombre = "Juan6";
 if (verificar_datos("[a-zA-Z]{6,10}", $nombre)) {
     echo "Los datos no coinciden con el formato solicitado";
 }
  */
+
+function limpiar_cadena($cadena)
+{
+    $cadena = trim($cadena);
+    $cadena = stripslashes($cadena);
+    $cadena = str_ireplace("<script>", "", $cadena);
+    $cadena = str_ireplace("</script>", "", $cadena);
+    $cadena = str_ireplace("<script src>", "", $cadena);
+    $cadena = str_ireplace("<script type=>", "", $cadena);
+    $cadena = str_ireplace("alert", "", $cadena);
+    $cadena = str_ireplace("SELECT * FROM", "", $cadena);
+    $cadena = str_ireplace("DELETE * FROM", "", $cadena);
+    $cadena = str_ireplace("INSERT INTO", "", $cadena);
+    $cadena = str_ireplace("DROP TABLE", "", $cadena);
+    $cadena = str_ireplace("DROP DATABASE", "", $cadena);
+    $cadena = str_ireplace("TRUNCATE TABLE", "", $cadena);
+    $cadena = str_ireplace("SHOW TABLES", "", $cadena);
+    $cadena = str_ireplace("SHOW DATABASES", "", $cadena);
+    $cadena = str_ireplace("<?php", "", $cadena);
+    $cadena = str_ireplace("?>", "", $cadena);
+    $cadena = str_ireplace("--", "", $cadena);
+    $cadena = str_ireplace("^", "", $cadena);
+    $cadena = str_ireplace("<", "", $cadena);
+    $cadena = str_ireplace("[", "", $cadena);
+    $cadena = str_ireplace("]", "", $cadena);
+    $cadena = str_ireplace("==", "", $cadena);
+    $cadena = str_ireplace(";", "", $cadena);
+    $cadena = str_ireplace("::", "", $cadena);
+    $cadena = trim($cadena);
+    $cadena = stripslashes($cadena);
+    return $cadena;
+}
+
+$texto = "  Hola <script>alert('Hola');</script>  ";
+echo limpiar_cadena($texto);
