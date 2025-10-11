e5c41e7d3fa271edfdf646389f072181c0b75494
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-10-11 09:29:57 -0500

    VALIDAR FORMULARIOS con EXPRESIONES REGULARES

--- php/main.php
+++ php/main.php
@@ -1,4 +1,29 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;dbname=db_inventario_php', 'root', '753159852');
+/*$pdo = new PDO('mysql:host=localhost;dbname=db_inventario_php', 'root', '753159852');
 $pdo->query("insert into categoria(categoria_nombre,categeria_ubicacion)
 values ('Prueba','Texto Ubicacion')");
+*/
+
+
+//conexion con la base de datos
+function conexion()
+{
+    $pdo = new PDO("mysql:host=localhost;dbname=db_inventario_php", "root", "");
+    return $pdo;
+}
+
+function verificar_datos($filtro, $cadena)
+{
+    //si la cadena coincide con el filtro retorna falso
+    if (preg_match("/^" . $filtro . "$/", $cadena)) {
+        return false;
+    } else {
+        return true;
+    }
+}
+/*
+$nombre = "Juan6";
+if (verificar_datos("[a-zA-Z]{6,10}", $nombre)) {
+    echo "Los datos no coinciden con el formato solicitado";
+}
+ */
