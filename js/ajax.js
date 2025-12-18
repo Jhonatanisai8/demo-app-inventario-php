ca4e1458ca4ed9b7284f83c75c1024e7a63b7263
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-12-17 23:11:33 -0500

    Implementar función AJAX para enviar formularios y manejar respuestas

--- js/ajax.js
+++ js/ajax.js
@@ -1,1 +1,34 @@
-console.log("Hola mundo desde ajax ")
+console.log("Hola mundo desde ajax ");
+const formularios_ajax = document.querySelectorAll(".formularioAjax");
+
+function enviar_formulario_ajax(e) {
+  e.preventDefault();
+  console.log("Ejecutando funcion enviar_formulario_ajax");
+  let enviar = confirm("Quieres enviar el formulario");
+  if (enviar === true) {
+    let data = new FormData(this);
+    let method = this.getAttribute("method");
+    let action = this.getAttribute("action");
+
+    let headers = new Headers();
+    let config = {
+      method: method,
+      headers: headers,
+      mode: "cors",
+      cache: "no-cache",
+      body: data,
+    };
+
+    fetch(action, config)
+      .then((response) => response.text())
+      .then((response) => {
+        let container = document.querySelector(".form-rest");
+        container.innerHTML = response;
+      });
+  } else {
+  }
+}
+
+formularios_ajax.forEach((formulario) => {
+  formulario.addEventListener("submit", enviar_formulario_ajax);
+});
