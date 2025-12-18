0b6050e8ef94ff0f6ab7c658e278f4ff5059d53b
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-12-17 23:37:59 -0500

    Actualizar vista de creación de usuario con nuevo diseño y estructura de formulario

--- views/user_new.php
+++ views/user_new.php
@@ -1,1 +1,53 @@
-<h1>Nuevo Usuario</h1>
+<div class="container is-fluid mb-6">
+    <h1 class="title">Usuarios</h1>
+    <h2 class="subtitle">Nuevo Usuario</h2>
+</div>
+
+<div class="container pb-6 pt-6">
+    <div class="form-rest mb-6 mt-6">
+    </div>
+    <form action="" class="formularioAjax" method="POST">
+        <div class="columns">
+            <div class="column">
+                <div class="control">
+                    <label>Nombres</label>
+                    <input type="text" class="input" name="usuario_nombre" required>
+                </div>
+            </div>
+            <div class="column">
+                <div class="control">
+                    <label>Apellidos</label>
+                    <input type="text" class="input" name="usuario_apellidos" required>
+                </div>
+            </div>
+        </div>
+
+        <div class="columns">
+            <div class="column">
+                <div class="control">
+                    <label>Usuario</label>
+                    <input type="text" class="input" name="usuario_usuario" required>
+                </div>
+            </div>
+            <div class="column">
+                <div class="control">
+                    <label>Email</label>
+                    <input type="email" class="input" name="usuario_email" required>
+                </div>
+            </div>
+        </div>
+
+        <div class="columns">
+            <div class="column is-half">
+                <div class="control">
+                    <label>Clave</label>
+                    <input type="password" class="input" name="usuario_clave" required>
+                </div>
+            </div>
+        </div>
+
+        <p class="has-text-centered mt-4">
+            <button type="submit" class="button is-info is-rounded">Guardar</button>
+        </p>
+    </form>
+</div>
