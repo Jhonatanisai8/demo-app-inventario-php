aac2367d9d2a0901c76ace68cc4bc983f7b3478c
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-12-21 14:13:49 -0500

    Incluir apellido en el saludo de bienvenida en home.php

--- views/home.php
+++ views/home.php
@@ -1,7 +1,8 @@
 <?php
 $nombre =  $_SESSION['nombre'];
+$apellido =   $_SESSION['apellido'];
 ?>
 <div class="container">
     <h1 class="title">Home</h1>
-    <h3 class="subtitle">Bienvenido <?php echo $nombre; ?></h3>
+    <h3 class="subtitle">Bienvenido <?php echo $nombre . " " . $apellido; ?></h3>
 </div>
