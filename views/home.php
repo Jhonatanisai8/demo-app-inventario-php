5d5d54e456ba0941d93bcef45aa1651eb8d98499
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-12-21 14:12:39 -0500

    Actualizar saludo en home.php para incluir el nombre del usuario

--- views/home.php
+++ views/home.php
@@ -1,4 +1,7 @@
+<?php
+$nombre =  $_SESSION['nombre'];
+?>
 <div class="container">
     <h1 class="title">Home</h1>
-    <h3 class="subtitle">Bienvenido!</h3>
+    <h3 class="subtitle">Bienvenido <?php echo $nombre; ?></h3>
 </div>
