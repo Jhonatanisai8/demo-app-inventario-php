c225b198a1815bf202f10294ce5a2c88ca87c7ba
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-12-21 14:06:56 -0500

    Refactor index.php for improved session handling and code organization

--- index.php
+++ index.php
@@ -3,29 +3,45 @@ require 'inc/session_start.php';
 ?>
 <!doctype html>
 <html lang="es">
+
 <head>
     <?php
     include 'inc/head.php';
     ?>
 </head>
-<body>
-<?php
 
-if (!isset($_GET['vista']) || $_GET['vista'] == "") {
-    $_GET['vista'] = "login";
-}
-if (is_file("./views/" . $_GET['vista'] . ".php")
-    && $_GET['vista'] != "login" && $_GET['vista'] != "404") {
-    include './inc/navbar.php';
-    include "./views/" . $_GET['vista'] . ".php";
-    include './inc/script.php';
-} else {
-    if ($_GET['vista'] == "login") {
-        include './views/login.php';
-    } else {
-        include './views/404.php';
+<body>
+    <?php
+
+    if (!isset($_GET['vista']) || $_GET['vista'] == "") {
+        $_GET['vista'] = "login";
     }
-}
-?>
+    if (
+        is_file("./views/" . $_GET['vista'] . ".php")
+        && $_GET['vista'] != "login" && $_GET['vista'] != "404"
+    ) {
+        //cerrar sesion forzada
+        if ((!isset($_SESSION['id']) || $_SESSION['id'] == "") ||
+            (isset($_SESSION['usuario']) || $_SESSION['usuario'] == "")
+        ) {
+            session_destroy();
+            if (headers_sent()) {
+                echo "<script> window.location.href='index.php?vista=login'; </script>";
+            } else {
+                header("Location: index.php?vista=login");
+            }
+        }
+        include './inc/navbar.php';
+        include "./views/" . $_GET['vista'] . ".php";
+        include './inc/script.php';
+    } else {
+        if ($_GET['vista'] == "login") {
+            include './views/login.php';
+        } else {
+            include './views/404.php';
+        }
+    }
+    ?>
 </body>
+
 </html>
